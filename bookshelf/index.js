7b09759f08a0ae79f275faec29ed4a6c83b14c27
kunxin-chor <chorkunxin@yahoo.com>
2022-04-14 08:26:47 +0000

    Reverted back to localhost mysql

--- bookshelf/index.js
+++ bookshelf/index.js
@@ -4,10 +4,10 @@
 const knex = require('knex')({
     client: 'mysql',  // which db tech are we using
     connection:{
-        user:'ckx_foobar',
-        password:'rotiprata123',
-        database:'ckx_organic',
-        host:"db4free.net"
+        user:'foo',
+        password:'bar',
+        database:'organic',
+        host:"localhost"
     }
 })
 
--- setup.sql
+++ setup.sql
@@ -1,3 +1,3 @@
-use ckx_organic;
+use organic;
 insert into categories (name) values ('Prepackaged'), ('Raw');
 insert into tags (name) values ("Healthy"),("Snack"),("Organic"),("Ethically Sourced")
